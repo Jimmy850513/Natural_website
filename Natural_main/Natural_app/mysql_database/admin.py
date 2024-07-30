8c58f8ffd38b8070f4ae8c95fcb29ace14033727
appmaker <JiangJimmy@dosuper.com.tw>
2024-07-30 10:49:18 +0800

    models配置、router配置、controller配置

--- Natural_main/Natural_app/mysql_database/admin.py
+++ Natural_main/Natural_app/mysql_database/admin.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Admin_User(models.Model):
     id = models.AutoField(primary_key=True)
-    account = models.CharField(null=False,unique=True)
-    password = models.CharField(null=False)
+    account = models.CharField(null=False,unique=True,max_length=50)
+    password = models.CharField(null=False,max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
